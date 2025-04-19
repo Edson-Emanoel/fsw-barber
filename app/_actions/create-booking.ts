b83f2f7718a34b798a1c4facffc7d8f0f869b420
Edson-Emanoel <edson.emanoelofc@gmail.com>
2025-04-19 13:54:34 -0300

    feat: garanti que apenas o usuário autenticado possa fazer uma reserva

--- app/_actions/create-booking.ts
+++ app/_actions/create-booking.ts
@@ -1,17 +1,22 @@
 "use server"
 
-import { revalidatePath } from "next/cache";
 import { db } from "../_lib/prisma"
+import { authOptions } from "../_lib/auth";
+import { revalidatePath } from "next/cache";
+import { getServerSession } from "next-auth";
 
 interface CreateBookingParams {
-    userId: string;
     serviceId: string;
     date: Date;
 }
 
 export const createBooking = async (params: CreateBookingParams) => { // Função server action
+    const user = await getServerSession(authOptions)
+    if(!user) {
+        throw new Error("Você não pode acessar essa rota, pois você precisa estar autenticado!! >:(")
+    }
     await db.booking.create({
-        data: params
+        data: {...params, userId: (user.user as any).id},
     })
     revalidatePath('/barbershops/[id]')
 }
